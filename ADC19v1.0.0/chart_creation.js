c1941feff83a5be1ac5124bf76c6ffe334681ed1
andreamariabellani <44612020+andreamariabellani@users.noreply.github.com>
2020-05-31 14:45:33 +0200

    modifiche

--- ADC19v1.0.0/chart_creation.js
+++ ADC19v1.0.0/chart_creation.js
@@ -1,3 +1,138 @@
+let seir_testy = [];
+let seir_test1y = [];
+let seir_test2y = [];
+let seir_test3y = [];
+let seir_testx = [];
+
+function create() {
+    getdatas();
+    getDataEsp();
+    getDataInfe();
+    getDataRecovered();
+    getDataSens();
+}
+
+function getdatas() {
+    $(document).ready(function () {
+        $.ajax({
+            url: "https://raw.githubusercontent.com/PieGroppelli/ADC19/master/seir_test.csv", // prendo i dati dal file raw di github
+            type: 'get',
+            dataType: 'text',
+            success: function (data) {
+
+                console.log("I got the data");
+
+                //lines è un array che contiene le ricghe della tabella tranne le prime due che sono quella della intestazione
+                let lines = data.split('\n').slice(2);
+
+
+                for (let i = 0; i < lines.length; i++) { //faccio il for per dividere le righe in colonne
+                    let temp = lines[i].split(";"); // divido la colonna
+
+                    if (!isNaN(parseFloat(temp[0])) &&
+                        !isNaN(parseFloat(temp[1])) &&
+                        !isNaN(parseFloat(temp[4])) &&
+                        !isNaN(parseFloat(temp[7])) &&
+                        !isNaN(parseFloat(temp[10]))) {
+                        //controllo di avere solo valori numerici e non lettere o altro
+
+                        seir_testx.push(parseFloat(temp[0])); // per l'asse x di tutti sempre uguale
+                        seir_testy.push(parseFloat(temp[1])); //Susceptible people
+                        seir_test1y.push(parseFloat(temp[4])); //Exposed people v
+                        seir_test2y.push(parseFloat(temp[7])); //Recovered people
+                        seir_test3y.push(parseFloat(temp[10])); //Infectious people
+                    }
+                }
+                createSeir();
+            },
+            error: function (jqXHR, textStatus, errorThrow) {
+
+                console.log("I failed to get the data");
+
+                console.log(textStatus);
+            }
+        });
+    });
+}
+
+function getDataEsp() { // per i grafici con indice varibile ho ogni 300 i dati per il nuovo grafico e avendone in totale 18 epr ogni elemento(esposti, guarti ...) ho pensato di creare una funzione che ritorni la y di quello richiesto dall'utente e con il linkk dato, così da avere una unica funzion
+    $(document).ready(function () {
+        $.ajax({
+            url: "https://raw.githubusercontent.com/PieGroppelli/ADC19/master/Exposed_people.csv",
+            type: 'get',
+            dataType: 'text',
+            success: function (data) {
+                let lines = data.split('\n').slice(3);
+                createEsposti(lines);
+            },
+            error: function (jqXHR, textStatus, errorThrow) {
+                console.log(textStatus);
+            }
+        });
+    });
+}
+function getDataInfe() { // per i grafici con indice varibile ho ogni 300 i dati per il nuovo grafico e avendone in totale 18 epr ogni elemento(esposti, guarti ...) ho pensato di creare una funzione che ritorni la y di quello richiesto dall'utente e con il linkk dato, così da avere una unica funzion
+    $(document).ready(function () {
+        $.ajax({
+            url: "https://raw.githubusercontent.com/PieGroppelli/ADC19/master/Infectious_people.csv",
+            type: 'get',
+            dataType: 'text',
+            success: function (data) {
+                let lines = data.split('\n').slice(3);
+                createInfetti(lines);
+            },
+            error: function (jqXHR, textStatus, errorThrow) {
+                console.log(textStatus);
+            }
+        });
+    });
+}
+function  getDataSens() {
+    $(document).ready(function () {
+        $.ajax({
+            url: "https://raw.githubusercontent.com/PieGroppelli/ADC19/master/Susceptible_people.csv",
+            type: 'get',
+            dataType: 'text',
+            success: function (data) {
+                let lines = data.split('\n').slice(3);
+                createSensibili(lines);
+            },
+            error: function (jqXHR, textStatus, errorThrow) {
+                console.log(textStatus);
+            }
+        });
+    });
+}
+
+function getDataRecovered() {
+    $(document).ready(function () {
+        $.ajax({
+            url: "https://raw.githubusercontent.com/PieGroppelli/ADC19/master/Recovered_people.csv",
+            type: 'get',
+            dataType: 'text',
+            success: function (data) {
+                let lines = data.split('\n').slice(3);
+                createRecovered(lines);
+            },
+            error: function (jqXHR, textStatus, errorThrow) {
+                console.log(textStatus);
+            }
+        });
+    });
+}
+
+
+function getDataSep(lines, n) {
+    let y =[];
+    let t = 300 * (n - 1);// indico con t il valore da cui devo aprtire all'interno del array lines con tutte le linee
+    for (let i = 0; i < 300; i++) {
+        let temp = lines[t].split(";");
+        y.push(parseFloat(temp[1]));
+        t++;
+    }
+    return y;
+}
+
 function createSeir() {
     var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
